9f44b3b2460ec4ca7da3f7e071f5f8eb37bdec57
Kaleb Bolack <kalebbolack14@gmail.com>
2023-09-10 20:49:31 -0600

    Completed all test's!

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = "Kaleb";
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
+
+console.log(welcomeMessage + excitedWelcomeMessage + shortGreeting)
